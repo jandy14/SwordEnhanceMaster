ef146655daafd69004fe9ec95429d8af656a2bed
jandy14_ <cbs3080@daum.net>
2016-01-31 22:57:35 +0900

    계속되는 실험 재료이미지 커지던 버그 수정

--- SwordEnhanceMaster/Build/Android/src/com/YourCompany/SwordEnhanceMaster/OBBData.java
+++ SwordEnhanceMaster/Build/Android/src/com/YourCompany/SwordEnhanceMaster/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-58720968L // the length of the file in bytes
+58867341L // the length of the file in bytes
 )
 };
 };
