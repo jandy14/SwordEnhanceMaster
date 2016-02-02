70fd8564fc758518b3e4f653b67aa6344c37f90b
jandy14_ <cbs3080@daum.net>
2016-02-02 22:22:15 +0900

    만족하기 힘들지만 144픽셀로 화면 크기 결정 무기 인벤토리 페이지 버그 수정 뒤로가기 버튼 이벤트 수정 사냥하러가기 버튼 이벤트 수정

--- SwordEnhanceMaster/Build/Android/src/com/YourCompany/SwordEnhanceMaster/OBBData.java
+++ SwordEnhanceMaster/Build/Android/src/com/YourCompany/SwordEnhanceMaster/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-58959281L // the length of the file in bytes
+59070791L // the length of the file in bytes
 )
 };
 };
