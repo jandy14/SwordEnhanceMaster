28275918f7d625a083cf218862bab5bd89ef12b4
zxc99as <zxc99as@naver.com>
2016-07-26 18:58:19 +0900

    * Scene 전환 시 터치 입력이 가능했던 버그 수정 * 강화할 무기 선택 시 이전에 사용했던 소재가 그대로 적용되던 버그 수정 * 무기 연구 및 연구 레벨 관련 스트립트 완료
    Former-commit-id: b542e4aa075b6f7e8271934bc9338c7e3da0a572

--- SwordEnhanceMaster 4.12/Build/Android/src/com/YourCompany/SwordEnhanceMaster/OBBData.java
+++ SwordEnhanceMaster 4.12/Build/Android/src/com/YourCompany/SwordEnhanceMaster/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-63291587L // the length of the file in bytes
+63327867L // the length of the file in bytes
 )
 };
 };
